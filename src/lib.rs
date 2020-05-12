9b0466637af0c7622019b41bc2e6354cb53c6614
Jacek Bilski <jacek@bilski.tech>
2020-05-12 22:52:43 +0200

    Extracted drawing the triangle. Also, enabled depth testing (not needed just yet, but as an exercise).

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ use web_sys::WebGl2RenderingContext;
 use crate::shader::Shader;
 
 mod shader;
+mod triangle;
 mod utils;
 
 #[wasm_bindgen(start)]
@@ -18,42 +19,15 @@ pub fn start() -> Result<(), JsValue> {
         .unwrap()
         .dyn_into::<WebGl2RenderingContext>()?;
 
+    context.enable(WebGl2RenderingContext::DEPTH_TEST);
+
     let shader = Shader::new(&context);
     context.use_program(Some(&shader.program));
 
-    let vertices: [f32; 9] = [-0.7, -0.7, 0.0, 0.7, -0.7, 0.0, 0.0, 0.7, 0.0];
-
-    let buffer = context.create_buffer().ok_or("failed to create buffer")?;
-    context.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, Some(&buffer));
-
-    // Note that `Float32Array::view` is somewhat dangerous (hence the
-    // `unsafe`!). This is creating a raw view into our module's
-    // `WebAssembly.Memory` buffer, but if we allocate more pages for ourself
-    // (aka do a memory allocation in Rust) it'll cause the buffer to change,
-    // causing the `Float32Array` to be invalid.
-    //
-    // As a result, after `Float32Array::view` we have to be very careful not to
-    // do any memory allocations before it's dropped.
-    unsafe {
-        let vert_array = js_sys::Float32Array::view(&vertices);
-
-        context.buffer_data_with_array_buffer_view(
-            WebGl2RenderingContext::ARRAY_BUFFER,
-            &vert_array,
-            WebGl2RenderingContext::STATIC_DRAW,
-        );
-    }
-
-    context.vertex_attrib_pointer_with_i32(0, 3, WebGl2RenderingContext::FLOAT, false, 0, 0);
-    context.enable_vertex_attrib_array(0);
-
     context.clear_color(0.0, 0.0, 0.0, 1.0);
-    context.clear(WebGl2RenderingContext::COLOR_BUFFER_BIT);
+    context.clear(WebGl2RenderingContext::COLOR_BUFFER_BIT | WebGl2RenderingContext::DEPTH_BUFFER_BIT);
 
-    context.draw_arrays(
-        WebGl2RenderingContext::TRIANGLES,
-        0,
-        (vertices.len() / 3) as i32,
-    );
+    triangle::draw_triangle(&context);
+
     Ok(())
 }
---
+++ src/triangle.rs
@@ -0,0 +1,37 @@
+use web_sys::WebGl2RenderingContext;
+
+pub fn draw_triangle(context: &WebGl2RenderingContext) {
+    let vertices: [f32; 9] = [-0.7, -0.7, 0.0, 0.7, -0.7, 0.0, 0.0, 0.7, 0.0];
+
+    let buffer = context
+        .create_buffer()
+        .expect("failed to create buffer");
+    context.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, Some(&buffer));
+
+    // Note that `Float32Array::view` is somewhat dangerous (hence the
+    // `unsafe`!). This is creating a raw view into our module's
+    // `WebAssembly.Memory` buffer, but if we allocate more pages for ourself
+    // (aka do a memory allocation in Rust) it'll cause the buffer to change,
+    // causing the `Float32Array` to be invalid.
+    //
+    // As a result, after `Float32Array::view` we have to be very careful not to
+    // do any memory allocations before it's dropped.
+    unsafe {
+        let vert_array = js_sys::Float32Array::view(&vertices);
+
+        context.buffer_data_with_array_buffer_view(
+            WebGl2RenderingContext::ARRAY_BUFFER,
+            &vert_array,
+            WebGl2RenderingContext::STATIC_DRAW,
+        );
+    }
+
+    context.vertex_attrib_pointer_with_i32(0, 3, WebGl2RenderingContext::FLOAT, false, 0, 0);
+    context.enable_vertex_attrib_array(0);
+
+    context.draw_arrays(
+        WebGl2RenderingContext::TRIANGLES,
+        0,
+        (vertices.len() / 3) as i32,
+    );
+}
